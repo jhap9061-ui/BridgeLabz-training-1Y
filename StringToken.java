5362d4d19435cfa7a856146d533188860ace136e
Prashant Jha <jhap9061@gmail.com>
2026-02-11 09:25:36 +0530

    Added: StringToken.java

---
+++ StringToken.java
@@ -0,0 +1,14 @@
+import java.util.StringTokenizer;
+public class StringToken {
+    static int a = 20;
+
+    static int addition(int a,int b)
+    {
+        return a+b;
+    }
+    public static void main(String[] args){
+        System.out.println(a);
+
+    }
+
+}
