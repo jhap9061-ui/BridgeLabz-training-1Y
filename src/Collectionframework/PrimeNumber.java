1419cb487d82e7d1c2427b3e8486656455aa6767
Prashant Jha <jhap9061@gmail.com>
2026-04-13 15:26:22 +0530

    Update PrimeNumber.java

--- src/Collectionframework/PrimeNumber.java
+++ src/Collectionframework/PrimeNumber.java
@@ -1,4 +1,30 @@
 package Collectionframework;
 
-public class PrimeNumber {
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        int start = 1, end = 100;
+
+        System.out.println("Prime numbers between 1 and 100:");
+
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    // Method to check if a number is prime
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
